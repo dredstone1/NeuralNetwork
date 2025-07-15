e9c5b81b196fe08c55808f342fa2c1992108883d
maayan <mayanpotu500@gmail.com>
2025-07-15 09:02:04 +0300

    adding time left

--- src/visualizer/Vstatus.cpp
+++ src/visualizer/Vstatus.cpp
@@ -17,15 +17,32 @@ void StatusPanel::doRender() {
 }
 
 std::string StatusPanel::getText() {
+	static float avgBatchPerSecond = 0.0f;
+	static int updatesCount = 0;
+
+	updatesCount++;
+	avgBatchPerSecond += (batchPerSecond - avgBatchPerSecond) / updatesCount;
+
 	std::ostringstream ss;
+
+	const int totalBatches = vstate->config.trainingConfig.getBatchCount();
+	const int currentBatch = vstate->currentBatch;
+	const int remainingBatches = totalBatches - currentBatch;
+
+	float minutesLeft = 0.0f;
+	if (avgBatchPerSecond > 0.01f) {
+		minutesLeft = remainingBatches / avgBatchPerSecond / 60.0f;
+	}
 
 	ss << TextLabels::CURRENT_PHASE_TEXT << NNmodeName[(int)vstate->nnMode.load()] << "\n"
 	   << TextLabels::RUNNING_MODE_TEXT << NNRunningModeName[vstate->settings.pause.load()] << "\n"
 	   << TextLabels::ALGORITHM_MODE_TEXT << algorithmName[(int)vstate->algorithmMode.load()] << "\n"
 	   << TextLabels::FPS_TEXT << fps << "/" << FPS_LIMIT << "\n"
-	   << TextLabels::CURRENT_BATCH_TEXT << vstate->currentBatch << "/" << vstate->config.trainingConfig.getBatchCount() << "#" << batchPerSecond << "\n"
+	   << TextLabels::CURRENT_BATCH_TEXT << currentBatch << "/" << totalBatches << " #" << batchPerSecond << "\n"
 	   << TextLabels::BATCH_SIZE_TEXT << vstate->config.trainingConfig.getBatchSize() << "\n"
-	   << TextLabels::LEARNING_RATE_TEXT << learningRate << "\n";
+	   << TextLabels::LEARNING_RATE_TEXT << learningRate << "\n"
+	   << "Time Left: " << std::fixed << std::setprecision(2) << minutesLeft << " min\n"
+	   << "Time Left: " << std::fixed << std::setprecision(2) << minutesLeft / 60 << " hours";
 
 	return ss.str();
 }
